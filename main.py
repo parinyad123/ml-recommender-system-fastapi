47c2589aaa384a817fc8867d6c2c672856ed2a73
parinyad123 <sci.equ@gmail.com>
2024-05-15 14:43:53 +0700

    add str_to_bool function

--- main.py
+++ main.py
@@ -89,6 +89,14 @@ ratings_df["movie"] = ratings_df["movieId"].map(movie2movie_encoded)
 
 movie_df = pd.read_csv(movies_csv)
 
+def str_to_bool(s):
+    truthy_values = {"true", "1", "yes"}
+    # falsy_values = {"false", "0", "no"}
+    s_lower = s.lower()
+    if s_lower in truthy_values:
+        return True
+    else:
+        return False
 
 app = FastAPI()
 
@@ -103,8 +111,11 @@ async def read_histories(user_id: int):
 
 
 
+
 @app.get("/recommendations/")
-async def read_item(user_id: int = 0, returnMetadata: bool = False):
+async def read_item(user_id: int = 0, returnMetadata: str = "False"):
+
+    returnMetadata = str_to_bool(returnMetadata)
 
     n_recommended_movie = 10
     movies_watched_by_user = ratings_df[ratings_df.userId == user_id]
